17ab5190b8f52756792fc29eca97052b69036397
juanmanuelvelasco <juanvelasco100@gmail.com>
2026-02-12 02:31:15 -0500

    bug

--- src/main/java/com/nequi/events/infrastructure/config/DynamoDbConfig.java
+++ src/main/java/com/nequi/events/infrastructure/config/DynamoDbConfig.java
@@ -1,20 +1,38 @@
 package com.nequi.events.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+import java.net.URI;
+
 @Configuration
 public class DynamoDbConfig {
+
+    @Value("${aws.dynamodb.endpoint}")
+    private String dynamoDbEndpoint;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.US_EAST_1) // Adjust region as needed
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .endpointOverride(URI.create(dynamoDbEndpoint))
+                .region(Region.of(awsRegion))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 
